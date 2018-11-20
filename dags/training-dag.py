fc9618248295997d9fe96021dbe1335aa658e742
wverheul <wverheul@bol.com>
2018-11-20 12:02:18 +0100

    Add test DAG for training

--- dags/training-dag.py
+++ dags/training-dag.py
@@ -16,9 +16,9 @@ with DAG(
     },
 ) as dag:
     t1 = PythonOperator(task_id='print_execution_date', python_callable=print_execution_date, provide_context=True, dag=dag)
-    t2 = BashOperator(task_id='wait_5', command='sleep 5')
-    t3 = BashOperator(task_id='wait_1', command='sleep 1')
-    t4 = BashOperator(task_id='wait_10', command='sleep 10')
+    t2 = BashOperator(task_id='wait_5', bash_command='sleep 5')
+    t3 = BashOperator(task_id='wait_1', bash_command='sleep 1')
+    t4 = BashOperator(task_id='wait_10', bash_command='sleep 10')
     t5 = DummyOperator(task_id='the_end')
 
 t1 >> [t2,t3,t4] >> t5
