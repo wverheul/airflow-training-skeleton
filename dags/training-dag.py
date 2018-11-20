ae557f4016e9b7b6c9f789b17a017124eeaf5a8f
wverheul <wverheul@bol.com>
2018-11-20 11:57:53 +0100

    Add test DAG for training

--- dags/training-dag.py
+++ dags/training-dag.py
@@ -1,6 +1,12 @@
 import airflow
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+import datetime
+
+def print_execution_date(execution_date, **context):
+    print(execution_date)
 
 with DAG(
     dag_id="airflow-training",
@@ -9,5 +15,10 @@ with DAG(
         "start_date": airflow.utils.dates.days_ago(3),
     },
 ) as dag:
-    t1 = DummyOperator(task_id='t1', dag=dag)
-    t2 = DummyOperator(task_id='t2', dag=dag)
+    t1 = PythonOperator(task_id='print_execution_date', python_callable=print_execution_date(), dag=dag)
+    t2 = BashOperator(task_id='wait_5', comman='sleep 5')
+    t3 = BashOperator(task_id='wait_1', comman='sleep 1')
+    t4 = BashOperator(task_id='wait_10', comman='sleep 10')
+    t5 = DummyOperator(task_id='the_end')
+
+t1 >> [t2,t3,t4] >> t5
